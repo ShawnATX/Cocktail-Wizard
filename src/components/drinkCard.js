ef05f42fd05ddd4cacee891a1bca00a617e54392
ShawnATX <volushawn@gmail.com>
2021-01-04 20:12:35 -0600

    ingredient parsing, added card reveal for video content

--- src/components/drinkCard.js
+++ src/components/drinkCard.js
@@ -5,6 +5,7 @@ const DrinkCard = (props) => {
         {
             drinkName:"",
             id:0,
+            //ingredients array is doublets with ingredient and measurement
             ingredients: [],
             instructions:"",
             image:""
@@ -18,6 +19,7 @@ const DrinkCard = (props) => {
     //let youtubeAPIKey = "AIzaSyC1DlLmv-ouNQJzBC-RC-jYzsLttiPumR0";
 
     useEffect(() => {
+        console.log("Use Effect");
         getRandomDrink()
     }, [])
     if (error) {
@@ -30,11 +32,9 @@ function getRandomDrink(){
         .then(res => res.json())
         .then(
             (response) => {
-                console.log(response);
-                //const reader = response.body.getReader();
-
                 const { idDrink, strDrink: drinkName, strInstructions: instructions, strDrinkThumb } = response.drinks[0];
                 let ingredientArr = buildIngredientsArray(response.drinks[0]);
+                console.log(ingredientArr);
                 setDrinkState({drinkName: drinkName, id: idDrink, ingredients: ingredientArr, instructions: instructions, image: strDrinkThumb});
             },
             (error) => {
@@ -64,12 +64,20 @@ function buildIngredientsArray(drinkObj) {
     let ingredient = "strIngredient" + i;
     let measurement = "strMeasure" + i;
     if (drinkObj[ingredient] === null) {
+        console.log(ingredientsArr);
       return ingredientsArr;
     }
     ingredientsArr.push([drinkObj[ingredient], drinkObj[measurement]]);
   }
 }
 
+function getIngredientList () {
+  const ingredientList = drinkState.ingredients.map((ingredient) => 
+    <li key={ingredient[0]}>{ingredient[0]} - {ingredient[1]}</li> 
+    );
+
+  return ingredientList
+}
 
 // function setVid(videoId) {
 //   let videoURL = "https://www.youtube.com/embed/";
@@ -86,13 +94,18 @@ return (
     <div class="row">
       <div class="col s12 m12 l12">
         <div class="card">
-          <div class="card-image">
-            <img src={drinkState.image} alt={drinkState.drinkName}/>
-            <span class="card-title">{drinkState.drinkName}</span>
-            <button href="" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></button>
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src={drinkState.image} alt={drinkState.drinkName}/>
+            <h1 class="card-title large">{drinkState.drinkName}</h1>
+            <button href="" class="btn-floating halfway-fab waves-effect waves-light red"><span><i class="material-icons">add</i></span></button>
           </div>
           <div class="card-content">
-            <p>{drinkState.instructions}</p>
+            {getIngredientList()}
+            <p class="flow-text">{drinkState.instructions}</p>
+          </div>
+          <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+            <p> All This Is Here Too!</p>
           </div>
         </div>
       </div>
